365ab786e6bae2b7c76b858d7e814012025332cc
Gabriel Tocha <gabriel.tocha@gmail.com>
2025-02-08 15:33:53 +0100

    wip

--- src/main.go
+++ src/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func HandleFileInput(filePath string) {
+	// --- load file into memory
+	file, err := os.ReadFile(filePath)
+	if err != nil {
+		fmt.Println("[ERROR]:", err)
+		return
+	}
+
+	fmt.Println("Data:", string(file))
+}
+
+func HandleReplInput() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print(">> ")
+
+		// --- handle empty input
+		if !scanner.Scan() {
+			fmt.Println("\nExiting...")
+			return
+		}
+
+		input := scanner.Text()
+		fmt.Println("Input:", input)
+	}
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+	args := os.Args
+
+	if len(args) > 2 {
+		panic("[ERROR]: Usage: golox [file_path]")
+	} else if len(args) == 2 {
+		HandleFileInput(args[1])
+	} else {
+		HandleReplInput()
+	}
 }
