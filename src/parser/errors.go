2e746289b4597e2f8614e96e3b399190aea4631b
Gabriel Tocha <gabriel.tocha@gmail.com>
2025-02-16 18:11:29 +0100

    error syncronization

--- src/parser/errors.go
+++ src/parser/errors.go
@@ -20,3 +20,28 @@ func NewParsingError(token lexer.Token, msg string) ParsingError {
 func (err ParsingError) Error() string {
 	return fmt.Sprintf("[ERROR]: parsing error at line %d: %s\n", err.Token.Line(), err.Msg)
 }
+
+/*
+Ideally, once a parsing error is encountered we would like to keep parsing.
+This allows us to report more errors to the user. The issue is that some gramatical
+invariant has already been violated, so the parser is now in a panic state.
+To allow (probably) enincumbered parsing, we try to skip to the next statement and continue
+parsing from there
+*/
+func (parser *Parser) synchronize() {
+	parser.next()
+
+	for !parser.isAtEnd() {
+		// if we hit a semicolon, this is likely the end of a statement
+		if parser.prev().TokenType() == lexer.SEMICOLON {
+			return
+		}
+
+		switch parser.prev().TokenType() {
+		case lexer.CLASS, lexer.FUN, lexer.VAR, lexer.FOR, lexer.IF, lexer.WHILE, lexer.PRINT, lexer.RETURN:
+			return
+		}
+
+		parser.next()
+	}
+}
